888a7e59f5c9dc909221854f9280445d6376a6c5
unkn0w_ind <100840597+unkn0wIND@users.noreply.github.com>
2023-09-17 11:38:58 +0200

    Update ListingInfo.tsx

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -50,7 +50,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                 items-center
                 gap-2
                 ">
-                    <div>Hosted by {user?.name}</div>
+                    <div>Hôte : {user?.name}</div>
                     <Avatar src={user?.image} />
                 </div>
                 <div className='flex flex-row items-center gap-4 font-light text-neutral-500'>
